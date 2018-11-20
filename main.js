2135387cc5333aa4bc8271a787d1a3980a01f642
Douglas Chalegre <douglas.chalegre@gmail.com>
2018-11-20 01:29:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
+var config = {
+    apiKey: "AIzaSyBbP1d0WC9pEz_M_Vq3KeG8kiIAuAjsZfA",
+    authDomain: "neuralnetjs.firebaseapp.com",
+    databaseURL: "https://neuralnetjs.firebaseio.com",
+    projectId: "neuralnetjs",
+    storageBucket: "neuralnetjs.appspot.com",
+    messagingSenderId: "377751663111"
+};
 
+//makes all buttons change the collor of the canvas after clicked
+const botoes = document.querySelectorAll('.btns');
+botoes.forEach(botao => {
+  botao.addEventListener("click", draw, false)
+});
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+//creates a random number from 1 to 255
 function randomColor(){
-    number = Math.random(); // gets a number from 0 to 1
-    number = number*255; // transforming it into rgb
-    number = Math.ceil(number); // ceils it so it is a int
+    number = Math.random(); 
+    number = number*255; 
+    number = Math.floor(number); 
     return number; 
 }
 
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {    
-      var ctx = canvas.getContext('2d');
+//creates a rgb number and return as a object
+function randomRGB(){
+  red = randomColor();
+  green = randomColor();
+  blue = randomColor();
+  return rgb = {
+    r:red,
+    g:green,
+    b:blue
+  };
+}
 
-      let r = randomColor();
-      let g = randomColor();
-      let b = randomColor();
-      ctx.fillStyle = `rgb(${r}, ${b}, ${g})`;
-      ctx.fillRect(0, 0, 100, 100);
-    }
+//draws in html a square with a random rgb color
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {    
+    var ctx = canvas.getContext('2d');
+    randomRGB();
+    console.log(rgb);
+    ctx.fillStyle = `rgb(${rgb.r}, ${rgb.b}, ${rgb.g})`; //rgb aparentemente tá errado
+    ctx.fillRect(0, 0, 150, 150);
   }
+}
+
