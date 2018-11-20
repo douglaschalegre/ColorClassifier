9609d3263a0af6eeab1240cb8323350dab45e576
Fernando Augusto <fernandoajneto@gmail.com>
2018-11-20 00:25:41 -0300

    fixing

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 
-function randomColor(){
-    number = Math.random(); // gets a number from 0 to 1
-    number = number*255; // transforming it into rgb
-    number = Math.ceil(number); // ceils it so it is a int
-    return number; 
+function randomColor() {
+  number = Math.random(); // gets a number from 0 to 1
+  number = number * 255; // transforming it into rgb
+  number = Math.ceil(number); // ceils it so it is a int
+  return number;
 }
 
 function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {    
-      var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
 
-      let r = randomColor();
-      let g = randomColor();
-      let b = randomColor();
-      ctx.fillStyle = `rgb(${r}, ${b}, ${g})`;
-      ctx.fillRect(0, 0, 100, 100);
-    }
+    let r = randomColor();
+    let g = randomColor();
+    let b = randomColor();
+    ctx.fillStyle = `rgb(${r}, ${b}, ${g})`;
+    ctx.fillRect(0, 0, 100, 100);
   }
+}
+
+const botoes = document.querySelectorAll('.btns');
+botoes.forEach(botao => {
+  botao.addEventListener("click", draw, false)
+})
