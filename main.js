c37e9376a1829fc0ec60382345657b29dc2da1c1
douglaschalegre <douglas.chalegre@gmail.com>
2018-11-21 21:39:11 -0300

    Implementando o tensorflow

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var config = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "xxxxxxx.firebaseapp.com",
-  databaseURL: "https://xxxxxxxx.firebaseio.com",
-  projectId: "xxxxxxxxxxx",
-  storageBucket: "xxxxxxxxxx.appspot.com",
-  messagingSenderId: "xxxxxxxxxxxxxxx"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+// var config = {
+//   apiKey: "xxxxxxxxxxxxxxxxxxxxxxxx",
+//   authDomain: "xxxxxxx.firebaseapp.com",
+//   databaseURL: "https://xxxxxxxx.firebaseio.com",
+//   projectId: "xxxxxxxxxxx",
+//   storageBucket: "xxxxxxxxxx.appspot.com",
+//   messagingSenderId: "xxxxxxxxxxxxxxx"
+// };
+// firebase.initializeApp(config);
+// var database = firebase.database();
 
 const botoes = document.querySelectorAll('.btns');
 botoes.forEach(botao => {
@@ -63,18 +63,86 @@ function draw() {
   }
 }
 
+labelList = [
+    'amarelo',
+    'laranja',
+    'vermelho',
+    'roxo',
+    'azul',
+    'verde',
+    'cinza',
+    'rosa',
+    'marrom'
+  ]
+
 var firebaseJson;
 let colors = [];
+let labels = [];
+let model;
+let xs,ys;
+
 $.getJSON("neuralnetjs.json", function(data){
   firebaseJson = data.cor;
+  
   Object.entries(firebaseJson).forEach(( record ) =>{
     col = [record[1].r/255, record[1].g/255, record[1].b/255];
     colors.push(col);
+    labels.push(labelList.indexOf(record[1].label));
+    });
+
+    // console.log(Object.entries(firebaseJson))
+    // console.log(colors);
+    // console.log(labels);
+    xs = tf.tensor2d(colors);
+    let labelTensor = tf.tensor1d(labels, 'int32');
+    // labelTensor.print();
+    ys = tf.oneHot(labelTensor, 9);
+    labelTensor.dispose();
+    // console.log(xs.shape);
+    // console.log(ys.shape);
+    // xs.print();
+    // ys.print();
+
+    model = tf.sequential();
+    let hidden = tf.layers.dense({
+      units: 16,
+      activation: 'sigmoid',
+      inputDim: 3
+    });
+    let output = tf.layers.dense({
+      units: 9,
+      activation: 'softmax'
+    });
+    model.add(hidden);
+    model.add(output);
+    
+    const learningRate = 0.2;
+    const optmizer = tf.train.sgd(learningRate);
+    model.compile({
+      optimizer: optmizer,
+      loss: 'categoricalCrossentropy'
+    });
+
+    async function train(){
+      const options = {
+        epochs: 2,
+        validationSplit: 0.1,
+        shuffle: true,
+        callbacks: {
+          onTrainBegin: () => console.log('training start'),
+          onTrainEnd: () => console.log('training complete'),
+          onEpochEnd: (num, logs) => {
+            console.log()
+          }
+        }
+      }  
+      return await model.fit(xs, ys, options)
+    }
+    train().then(results => {
+      console.log(results);
     });
 });
-console.log(colors);
-let xs = tf.tensor3d(colors);
-console.log(xs.shape);
+
 
 
 
